52c666e13321550a3f63fc9fc34cd3370598bf8d
munlikaCh <munlika_ch@cmu.ac.th>
2021-09-17 01:10:56 +0700

    1.4

--- src/main/java/se331/lab/rest/repository/EventRepository.java
+++ src/main/java/se331/lab/rest/repository/EventRepository.java
@@ -12,4 +12,6 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<Event,Long> {
     List<Event> findAll();
     Page<Event> findByTitle(String title, Pageable pageRequest);
+
+    Page<Event> findByTitleContaining(String title, Pageable pageRequest);
 }
